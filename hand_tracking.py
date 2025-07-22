425547633bc66ae19fa3f23324efec1112d17795
hma423 <henryma2026@u.northwestern.edu>
2025-07-22 00:21:44 -0500

    added fingertips dictionary

--- hand_tracking.py
+++ hand_tracking.py
@@ -4,18 +4,30 @@ import cv2
 print("************************** START OF PROGRAM ****************************")
 print("\n\npress q to quit\n")
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1200)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1200)
 
 hands = mp.solutions.hands
 hand = hands.Hands()
 drawing = mp.solutions.drawing_utils
+
+
+fingertips = {
+    4: '1',   # Thumb
+    8: '2',   # Index
+    12: '3',  # Middle
+    16: '4',  # Ring
+    20: '5'   # Pinky
+}
+
+
 
 while True:
     success, frame = cap.read()
     if success:
         RGB_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         result = hand.process(RGB_frame)
+        
         if result.multi_hand_landmarks:
             for landmark in result.multi_hand_landmarks:
                 print(landmark) 

83b0bad2250472866725812e6cdc84836d0b6971
hma423 <henryma2026@u.northwestern.edu>
2025-07-23 00:49:26 -0500

    pinching working

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,5 +1,6 @@
 import mediapipe as mp 
 import cv2 
+import math
 
 print("************************** START OF PROGRAM ****************************")
 print("\n\npress q to quit\n")
@@ -12,16 +13,6 @@ hand = hands.Hands()
 drawing = mp.solutions.drawing_utils
 
 
-fingertips = {
-    4: '1',   # Thumb
-    8: '2',   # Index
-    12: '3',  # Middle
-    16: '4',  # Ring
-    20: '5'   # Pinky
-}
-
-
-
 while True:
     success, frame = cap.read()
     if success:
@@ -30,9 +21,22 @@ while True:
         
         if result.multi_hand_landmarks:
             for landmark in result.multi_hand_landmarks:
-                print(landmark) 
                 drawing.draw_landmarks(frame, landmark, hands.HAND_CONNECTIONS)
+                h, w, _ = frame.shape
                 
+
+                thumb_tip = landmark.landmark[4]
+                pointer_finger_tip = landmark.landmark[8]
+
+                normalized_thumb = (int (thumb_tip.x * w) , int (thumb_tip.y * h))
+                normalized_pointer = (int ( pointer_finger_tip.x * w) , int ( pointer_finger_tip.y * h))
+
+                if math.dist(normalized_thumb, normalized_pointer) < 80:
+                    mid_x, mid_y = (normalized_thumb[0] + normalized_pointer[0]) // 2, (normalized_thumb[1] + normalized_pointer[1]) // 2
+                    cv2.circle(frame, (mid_x, mid_y), 15, (0, 0, 255), -1)  # Red dot for pinch
+                    cv2.putText(frame, 'Pinching', (mid_x - 30, mid_y - 30), cv2.FONT_HERSHEY_SIMPLEX, 
+                                0.6, (0, 0, 255), 2)
+
         cv2.imshow("capture image", frame)
         if cv2.waitKey(1)  == ord('q'):
             break
